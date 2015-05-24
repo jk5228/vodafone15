4f2486492fb61845cfba5742f83b2cae6a786f63
Charlie Proctor <charlie@charlieproctor.com>
2015-05-24 13:26:39 +0000

    use request

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+var request = require('request');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  request('http://www.google.com', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    res.send(body) // Show the HTML for the Google homepage.
+	  }
+	})
 });
 
 var server = app.listen(3000, function () {
