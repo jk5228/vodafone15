08971d2a2c768b89e0cf47e1174513107aab165c
Charlie Proctor <charlie@charlieproctor.com>
2015-05-24 13:30:45 +0000

    adding api key

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 var request = require('request');
+var apiKey = "NLNUbwXn31cUCJOuJrnQpw=="
 
 app.get('/', function (req, res) {
   request('http://www.google.com', function (error, response, body) {
+
 	  if (!error && response.statusCode == 200) {
 	    res.send(body) // Show the HTML for the Google homepage.
 	  }
@@ -19,3 +21,4 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
+
